045ac6c72ea53a3ca6ff2dc58779c75d65e6e4df
Joseph Joestar <skylanders024@gmail.com>
2020-10-29 13:14:49 -0400

    Add project files.

---
+++ Serialization1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization1
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double SSNumber { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ Serialization1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Serialization1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.ID = 123;
+            emp.Name = "Ninja Nerd In Training";
+            emp.SSNumber = 153638465;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: {emp.Name} \nEmployee SS#: {emp.SSNumber} \nDate Created: {emp.EntryDate}");
+
+
+            string FilePath = "C:/Temp/";
+            string FileName = "Ninja.Xml";
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            XmlSerializer ser = new XmlSerializer(typeof(Employee));
+
+            ser.Serialize(writer, emp);
+            writer.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
